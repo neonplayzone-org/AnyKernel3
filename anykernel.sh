fbff4866a02e8d70e1fb17b015d44da4c58bf348
Nauval Rizky <enuma.alrizky@gmail.com>
2023-07-09 09:09:07 +0700

    AnyKernel3: Fix patching custom MIUI phyd
    Changes:
    v1: xiaomi hw composer is dividing pyh dimension by around 10, let's
        mitigate it.
    v2: For some reason, sed in-place editing doesn't work, so we had to do manual
        pipe and overwrite the header file.
    
    Signed-off-by: Nauval Rizky <enuma.alrizky@gmail.com>
    Signed-off-by: wimbiyoashizkia <wimbiyoashizkia@yahoo.com>

--- anykernel.sh
+++ anykernel.sh
@@ -45,6 +45,41 @@ if [ ! -z "$android_ver" ]; then
 fi
 #end checker android version
 
+## Custom MIUI patch (DO NOT CHANGE)
+# begin checker custom miui patch
+cmdl_add() {
+    local fh=$split_img/header
+    local fhmod=$split_img/header.mod
+
+    if ! grep "$1" ; then
+        cat $fh | sed -E "s/cmdline=(.*)/cmdline=\1 $1/" > $fhmod
+        mv $fhmod $fh
+    fi
+}
+
+cmdl_rm() {
+	local fh=$split_img/header
+    local fhmod=$split_img/header.mod
+
+    if grep "$1" $fh; then
+        cat $fh | sed -E "s/ $1//" $fh > $fhmod
+        mv $fhmod $fh
+    fi
+}
+
+patch_mi() {
+    # cleanup it first
+    cmdl_rm msm_dsi.phyd_miui=1
+
+    local vi=$(file_getprop /system/build.prop ro.system.build.version.incremental)
+    if contains "$ZIPFILE" "miui.zip" || contains "$vi" "V13." || contains "$vi" "V14." ; then
+        ui_print "MIUI is detected: $vi";
+        ui_print "Enabling msm_dsi.phyd_miui for MIUI compatibility...";
+        cmdl_add msm_dsi.phyd_miui=1
+    fi
+}
+#end checker custom miui patch
+
 write_boot; # use flash_boot to skip ramdisk repack, e.g. for devices with init_boot ramdisk
 ## end boot install
 
